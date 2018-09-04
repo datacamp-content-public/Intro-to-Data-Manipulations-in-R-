1b92284823ae494b792812209673c5d3d6540df6
carpio-ucv <carpio_ucv@yahoo.es>
2018-09-04 17:22:06 +0100

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -6,7 +6,7 @@ library(remotes)
 #install.packages("reshape2")
 devtools::install_version("reshape2", "1.4.3")
 devtools::install_version("dplyr", "0.7.5")
-install_version("data.table", "1.11.4")
+#install_version("data.table", "1.11.4")
 #devtools::install_version("tidyr", "0.8.1")
 #install.packages("reshape2")
 #devtools::install_version("data.table", "1.11.4")
@@ -27,12 +27,12 @@ data_dir <- "/usr/local/share/datasets"
 dir.create(data_dir)
 
 #' @param x Amazon S3 URL of the dataset, as a string. 
-download_data <- function(x) {
-  download.file(
-    x,
-    file.path(data_dir, basename(x))
-  )
-}
+#download_data <- function(x) {
+ # download.file(
+  #  x,
+   # file.path(data_dir, basename(x))
+  #)
+#}
 
 # Usage is, e.g.,
 # download_data(
